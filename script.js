f5b16c1af1149944059b9cdd3c1f8ab2bf2f82f0
rat182 <>
2022-06-26 07:01:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+  function myFunc(){
+  window.location.href = "mantaray.html"
+}
+
+  function myFunc2(){
+  window.location.href = "green_sea_turtle.html"
+}
+
+  function myFunc3(){
+  window.location.href = "dugong.html"
+}
+
+  function myFunc4(){
+  window.location.href = "whale_shark.html"
+}
+
+  function myFunc5(){
+  window.location.href = "clownfish.html"
+}
+
+  function myFunc6(){
+  window.location.href = "mantis_shrimp.html"
+}
+
+ function myFunc7(){
+  window.location.href = "index.html"
+}
